9bb437817f981a4b5de1bfe3699c97c537ba7052
hrivierefr <legsmower@gmail.com>
2018-10-03 18:50:58 +0200

    Ajout des exercicesRuby crypto et journalist

---
+++ journalist.rb
@@ -0,0 +1,129 @@
+class DataTraining
+
+	attr_accessor :array, :char, :string
+
+	#Détection des occurences contenant des chiffres
+
+	def figures_detection (array)
+		with_figures  = Array.new
+		without_figures  = Array.new
+
+		array.each do |handle|
+			if handle.delete("0-9") != handle
+				with_figures << handle
+			else  
+				without_figures << handle
+			end
+		end
+
+		return(with_figures.length)
+
+#		end
+	end
+
+#Détection des occurrences contenant une string donnée
+
+	def string_detection (array, string)
+		with_string  = Array.new
+		without_string  = Array.new
+
+		string=string.upcase
+
+		array.each do |handle|
+			if handle.upcase.index(string) != nil
+				with_string << handle
+			else  
+				without_string << handle
+			end
+		end
+
+		return (with_string.length)
+
+#		puts "#{with_string.length} éléments de cet array contiennent la string '#{string}'. Voulez-vous les afficher (y/n) ?"
+#		print "> "
+#		if gets.chomp == "y"
+#			puts with_string
+#		end
+	end
+
+#Détection majuscules en début de pseudo(ne gère pas les handles avec _ en handle[1])
+
+	def capitalize_detection (array)
+		with_capitalize = Array.new
+		without_capitalize = Array.new
+
+		array.each do |handle|
+			if handle[1].upcase == handle[1]
+				with_capitalize << handle
+			else
+				without_capitalize << handle
+			end
+		end
+
+		return with_capitalize.length
+#		puts "#{with_uppercase.length} de ces éléments commencent par une majuscule. Voulez-vous les afficher (y/n) ?"
+#		print "> "
+#		if gets.chomp == "y"
+#			puts with_uppercase
+#		end
+	end
+
+#Détection majuscules (ne gère pas les handles avec _ en handle[1])
+
+	def uppercase_detection (array)
+		with_uppercase = Array.new
+		without_uppercase = Array.new
+
+		array.each do |handle|
+			if handle[1].upcase == handle[1]
+				with_uppercase << handle
+			else
+				without_uppercase << handle
+			end
+		end
+
+		return with_uppercase.length
+#		puts "#{with_uppercase.length} de ces éléments commencent par une majuscule. Voulez-vous les afficher (y/n) ?"
+#		print "> "
+#		if gets.chomp == "y"
+#			puts with_uppercase
+#		end
+	end
+
+	def char_count (array,char)
+		with_char = Array.new
+		without_char = Array.new
+		count = 0
+
+		array.each do |handle|
+			if handle.count char > 0
+				count += handle.count char
+				with_char << handle
+			else
+				without_char << handle
+			end
+		end
+
+		return count
+
+#		puts "Cet array contient #{count} fois #{char}. Voulez-vous afficher les handle (y/n) ?"
+#		print "> "
+#		if gets.chomp == "y"
+#			puts with_char
+#		end
+	end
+
+	def perform (array,string,char)
+
+		puts "Cette liste contient #{array.length} handles, dont :"
+		puts "- #{figures_detection(array)} contenant des chiffres."
+#		puts "- #{string_detection (array, string))} contenant '#{@string}'."
+		puts "- #{capitalize_detection (array)} commençant par une majuscule."
+#		puts "- #{char_count (array,char)} '#{char}', tous handle confondus."
+	end
+end
+
+journalists = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+DataTraining.new.perform(journalists,"aude",'_')
+
+#puts journalists.sort
